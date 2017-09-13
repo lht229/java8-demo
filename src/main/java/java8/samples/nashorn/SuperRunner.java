94096cce3b2ff88bcf8026327f06a6117938fb2d
lht229 <lht1026307556@163.com>
2017-09-13 11:39:06 +0800

    初始化

---
+++ src/main/java/java8/samples/nashorn/SuperRunner.java
@@ -0,0 +1,16 @@
+package java8.samples.nashorn;
+
+/**
+ *
+ *
+ * @author:haitao.liu
+ *
+ */
+public class SuperRunner implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("super run");
+    }
+
+}
---
+++ src/main/java/java8/samples/stream/Optional1.java
@@ -0,0 +1,23 @@
+package java8.samples.stream;
+
+import java.util.Optional;
+
+/**
+ *
+ *
+ * @author:haitao.liu
+ *
+ */
+public class Optional1 {
+
+    public static void main(String[] args) {
+        Optional<String> optional = Optional.of("bam");
+
+        optional.isPresent();           // true
+        optional.get();                 // "bam"
+        optional.orElse("fallback");    // "bam"
+
+        optional.ifPresent((s) -> System.out.println(s.charAt(0)));     // "b"
+    }
+
+}
